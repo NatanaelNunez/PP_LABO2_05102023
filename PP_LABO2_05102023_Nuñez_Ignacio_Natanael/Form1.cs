b2c45d2b4f733595a06b37a12d456288f3c0efd1
Natanael <natanael.esc5@gmail.com>
2023-10-04 20:01:31 -0300

    Add project files.

---
+++ PP_LABO2_05102023_Nuñez_Ignacio_Natanael/Form1.cs
@@ -0,0 +1,10 @@
+namespace PP_LABO2_05102023_Nuñez_Ignacio_Natanael
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
